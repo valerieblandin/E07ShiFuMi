1f9cafb439ed7b2607f7c867267f44d927d95191
valerieblandin <valerie.blandin@yahoo.fr>
2021-05-13 18:14:04 +0200

    update

--- ShiFuMi/assets/js/script.js
+++ ShiFuMi/assets/js/script.js
@@ -1,8 +1,6 @@
-let game = $('#gameArena')
-game.hide()
-
 $('#btnBegin').click(function(){
-    $('#gameArena').show()
+    $('#gameAccueil').hide()
+    $('#pageFin').hide()
 })
 
 //Déclaration compteurs de score
@@ -10,7 +8,7 @@ let victoireJoueur = 0
 let victoireOrdi = 0
 
 //Déclenchement par boutons pour la version mobile
-$(".btn").on('click', function(){   
+$("#zoneChoix button").on('click', function(){   
     choixJoueur = $(this).attr('class');
     // Choix Ordinateur
     console.log(`${choixJoueur}`)
@@ -30,22 +28,13 @@ $(".btn").on('click', function(){
                 affichageOrdi()
                                     }, 3000)
             setTimeout(function(){  
-                reset()
-                
+                reset()                
                                     }, 5000)
 })
 
-//Effets produits a l'enfoncement du click (remise a ezéro de la zone de jeu)
+//Effets produits a l'enfoncement du click (remise a zéro de la zone de jeu)
 $(".drag").on('mousedown', function(){
-    $('#timer').removeClass('on')
-    $('#choixOrdi p').text('?')
-    $('#choixOrdi').removeClass('cetoine rose main')
-    $('#choixJoueur').removeClass('cetoine rose main')
-    $('#ordiAnimation').removeClass('win eq loose')
-    $('#playerAnimation').removeClass('win eq loose')
-    $('#ordiAnimation').removeClass('cetoine rose main')
-    $('#playerAnimation').removeClass('cetoine rose main')
-    $('#gameArena').removeClass(`score`)
+    reset()
 })
 
 //Effets produits au relachement du click
@@ -80,15 +69,29 @@ $(function() {
     })
 })
 
-//Détermination du vaiqueur
+//Détermination du vainqueur
 function resultat() {
     if (choixJoueur === 'rose' &&  choixOrdinateur === 'main' ||
             choixJoueur === 'main' &&  choixOrdinateur === 'cetoine' ||
             choixJoueur === 'cetoine' &&  choixOrdinateur === 'rose') {
                 victoireJoueur+= 1
                 $('#victoireJoueur').text(`${victoireJoueur}`)
-                if (victoireJoueur === 3) {           
-                    console.log(`Joueur Gagne`)
+                if (victoireJoueur === 3) { 
+                    //Ajout de classe egalité pour l'animation
+                    $('#ordiAnimation').addClass(`loose ${choixOrdinateur}`)
+                    $('#playerAnimation').addClass(`win ${choixJoueur}`)
+                    //Déclencheur animation Score
+                    $('#gameArena').addClass(`score`)
+                    //Annonce du résultat
+                    $('#resultat p').text(`Tu as gagné`)
+                    $('#victoireJoueur').text(`${victoireJoueur}`)
+                    $('#pageFin p').text(`Youpiiiii ! Tu as gagné ! ${victoireJoueur} à ${victoireOrdi}`)
+                    setTimeout(function(){  
+                        $('#gameArena').hide()
+                        $('#gameAccueil').hide()
+                        $('#pageFin').show()
+                                            }, 3000)
+
                 } 
                 else {
                     //Ajout de classe egalité pour l'animation
@@ -97,7 +100,7 @@ function resultat() {
                     //Déclencheur animation Score
                     $('#gameArena').addClass(`score`)
                     //Annonce du résultat
-                    $('#resultat p').text(`Vous avez gagné`)
+                    $('#resultat p').text(`Tu as gagné`)
                     $('#victoireJoueur').text(`${victoireJoueur}`)
                 }
     } else if (choixJoueur === choixOrdinateur) {
@@ -112,7 +115,20 @@ function resultat() {
         victoireOrdi+=1
         $('#victoireOrdi').text(`${victoireOrdi}`)
             if (victoireOrdi === 3) {
-                console.log(`Ordinateur gagne la partie`)
+                            //Ajout de classe egalité pour l'animation
+                $('#ordiAnimation').addClass(`win ${choixOrdinateur}`)
+                $('#playerAnimation').addClass(`loose ${choixJoueur}`)
+                $('#gameArena').addClass(`score`)
+                //Annonce du résultat
+                $('#resultat p').text(`C'est perdu`)
+                $('#victoireOrdi').text(`${victoireOrdi}`)
+                $('#pageFin p').text(`Bouuuuh ! Tu as perdu ! ${victoireJoueur} à ${victoireOrdi}`) 
+                setTimeout(function(){  
+                    $('#gameArena').hide()
+                    $('#gameAccueil').hide()
+                    $('#pageFin').show() 
+                                        }, 3000)
+
             } 
             else {
             //Ajout de classe egalité pour l'animation
@@ -141,9 +157,28 @@ function affichageOrdi() {
         $('#choixOrdi').addClass('cetoine')
     }
 }
+// new game
+$('#btnRetour').click(function(){
+    reset()
+    $('#pageFin').hide()
+    $('#gameArena').show()
+    // reset fin de partie
+    victoireJoueur = 0
+    $('#victoireJoueur').text(`${victoireJoueur}`)
+    victoireOrdi = 0
+    $('#victoireOrdi').text(`${victoireOrdi}`)
+})
+// reset pendant la partie
+function reset(){
+    $('#timer').removeClass('on')
+    $('#choixOrdi p').text(`Que va choisir Kiki l'ordi?`)
+    $('#choixJoueur p').text(``)
+    $('#choixOrdi').removeClass('cetoine rose main')
+    $('#choixJoueur').removeClass('cetoine rose main')
+    $('#ordiAnimation').removeClass('win eq loose')
+    $('#playerAnimation').removeClass('win eq loose')
+    $('#ordiAnimation').removeClass('cetoine rose main')
+    $('#playerAnimation').removeClass('cetoine rose main')
+    $('#gameArena').removeClass(`score`)
+}
 
-/* reset de jeu
-victoireJoueur = 0
-$('#victoireJoueur').text(`${victoireJoueur}`)
-victoireOrdi = 0
-$('#victoireOrdi').text(`${victoireOrdi}`)    */
